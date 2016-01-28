ce45d33d07b301bce36c87ee4fb5cf4530e3edc0
CyrusWang <CyrusWang@Cyruss-MacBook-Pro-2.local>
2016-01-28 16:19:42 -0600

    More structure changes

--- class_header.h
+++ class_header.h
@@ -8,12 +8,10 @@ public:
 	Symbol(char* s);					// Constructor
 	~Symbol();							// Destructor
 	Symbol* Next_bucket();
-	Symbol* Next_symbol();
 
 private:
 	char* name;							// pointer of the variable
-	Symbol* next_bucket;				// next same name variable (different scopes)
-	Symbol* next_symbol;				// next variable
+	Symbol* next_bucket;				// next collinsion variable
 	/*
 	 	Attributes
 		Add new attributes to the end
@@ -28,14 +26,15 @@ public:
 	Symbol* Empty_table();				// empty current table	
 	Symbol* Insert_symbol(char* s);		// insert a new symbol
 	Symbol* Search_symbol(char* s);		// search symbol
-	Symbol* Head_symbol() { return head; }	//return the head symbol
-	Symbol* Tail_symbol() { reutrn tail; }	//return the tail symbol
-	unsigned int Count_table_symbols();
+	//Symbol* Head_symbol() { return head; }	//return the head symbol
+	//Symbol* Tail_symbol() { reutrn tail; }	//return the tail symbol
+	unsigned int Count_table_symbols() { return count;} // return number of symbols in the table
 private:
 	Symbol* hashtable[HT_SIZE];			// define hashtable size
-	Symbol* head;						// head symbol of the hashtable(first inserted)
-	Symbol* tail;						// tail symbol of the hashtable(last inserted)
+	//Symbol* head;						// head symbol of the hashtable(first inserted)
+	//Symbol* tail;						// tail symbol of the hashtable(last inserted)
 	unsigned int count;					// number of symbols in the hashtable
+	Scope* upper;
 };
 
 class Symbol_Table {					// list of scopes (hashtables)
@@ -43,10 +42,11 @@ public:
 	Symbol_Table();						// constructor
 	Scope* Enter_new_scope();			// enter new scope ( add a new scope to the head of the list)
 	Scope* Leave_current_scope();		// exit the scope (remove current scope from the list)
+	Scope* Currrent_scope();			// return pointer of current scope
 
 private:
+	Symbol_Table* table;				// pointer of the symboltable
 	Scope* current;						// current scope
-	Scope* upper;						// upper scope
 	unsigned int level;					// number of scopes in the list
 };
 
