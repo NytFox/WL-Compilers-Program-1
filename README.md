31bfc72faec5676116c3cdba808c592a63eccceb
Cyrus Wang <cyrus.wang.xinyue@gmail.com>
2016-02-01 19:03:32 -0600

    version 1.5

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# WL-Compilers-Program-1
+*****************************************************
+
+WL-Compilers-Program-1 Read Me
+
+*****************************************************
+Description:
+
+	This is a simple implementation of a symbol table with hash tables
+	Scopes are connected as a stack of hashtables
+	Symbols are stored in the hashtable and collinsions are dealt with linked list 
+
+Files:
+class_header.h 					: class declaration
+class_implementation.cpp 		: class functions
+main.cpp 						: test functions
+
+Running instruction:
+
+Environment						: Linux
+Command  						: 1) g++ *.cpp
+								  2) ./a.out
+
+Notice: warnings would be thrown out due to string conversion not enabled by defualt. As testing purpose, it doesn't influence the result.
--- main.cpp
+++ main.cpp
@@ -5,45 +5,58 @@ using namespace std;
 int main(){
 	Symbol_Table* new_table;
 	new_table = new Symbol_Table;
+	cout <<"New scope created"<<endl;
 	new_table -> Insert_symbol("a","int","5");
+	cout << "Symbol: ' int a 5 ' is inserted to a current scope"<<endl;
+	new_table -> Insert_symbol("b","string","abc");
+	cout << "Symbol: ' string b abc ' is inserted to a current scope"<<endl;
 	Scope* t;
+	cout << "Searching symbol 'a' "<<endl;
 	if(t = new_table -> Search_symbol("a")){
-		cout<<"name searched"<<endl;
-		char* tmp=t -> Search_symbol("a") -> type;
-		cout<<tmp<<endl;
+		cout<<"symbol detected:"<<endl;
+		cout<<"Type:"<<t -> Search_symbol("a") -> type<<endl;
+		cout<<"Value:"<<t -> Search_symbol("a") -> value<<endl;
 	}
 	else{
-		cout<<"no found"<<endl;
+		cout<<"not found"<<endl;
 	}
-	cout<<"level:"<<new_table->level<<endl;
 
 	new_table -> Enter_new_scope();
-
+	cout <<"Enter next scope"<<endl;
 	new_table -> Insert_symbol("a","char","10");
+	cout <<"Symbol: ' char a 10 ' is inserted to a new scope"<<endl;
+	cout << "Searching symbol 'a' "<<endl;
 	if(t = new_table -> Search_symbol("a")){
-		cout<<"name searched"<<endl;
-		char* tmp=t -> Search_symbol("a") -> type;
-		cout<<tmp<<endl;
+		cout<<"symbol detected:"<<endl;
+		cout<<"Type:"<<t -> Search_symbol("a") -> type<<endl;
+		cout<<"Value:"<<t -> Search_symbol("a") -> value<<endl;
 	}
 	else{
-		cout<<"no found"<<endl;
+		cout<<"not found"<<endl;
 	}
-
+	cout << "Searching symbol 'b' (upper scope)"<<endl;
+	if(t = new_table -> Search_symbol("b")){
+		cout<<"symbol detected:"<<endl;
+		cout<<"Type:"<<t -> Search_symbol("b") -> type<<endl;
+		cout<<"Value:"<<t -> Search_symbol("b") -> value<<endl;
+	}
+	else{
+		cout<<"not found"<<endl;
+	}
+	cout << "Leave current scope"<<endl;
 	new_table -> Leave_current_scope();
+
 	new_table -> Insert_symbol("a","bool","100");
+	cout <<"Symbol: ' bool a 100 ' is inserted to a new scope"<<endl;
+	cout << "Searching symbol 'a' (collinsion test, suppose to get new inserted)"<<endl;
 	if(t = new_table -> Search_symbol("a")){
-		cout<<"name searched"<<endl;
-		char* tmp=t -> Search_symbol("a") -> type;
-		cout<<tmp<<endl;
+		cout<<"symbol detected:"<<endl;
+		cout<<"Type:"<<t -> Search_symbol("a") -> type<<endl;
+		cout<<"Value:"<<t -> Search_symbol("a") -> value<<endl;
 	}
 	else{
-		cout<<"no found"<<endl;
+		cout<<"not found"<<endl;
 	}
-
-	cout<<"test4"<<endl;
-
-
-
 
 	return 0;
 }
